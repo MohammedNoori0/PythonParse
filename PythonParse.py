c6030935294076aafcf19fff664a7bde5bf05b4f
MohammedNoori0 <noorimohammed076@gmail.com>
2024-08-17 16:19:14 -0400

    Finished Program

---
+++ PythonParse.py
@@ -0,0 +1,18 @@
+import re
+
+
+#writing the function of parsing 
+def parse_log(log_file):
+    with open(log_file, 'r') as f:
+        for line in f:
+            if re.search(r'failed|error|unauthorized|malicious', line, re.IGNORECASE):
+                print(f"Suspicious Activity: {line}")
+
+
+
+# storing the variable 
+log_file = 'system_logs.log'
+
+#executing
+parse_log(log_file)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Program to help parsing logs.
